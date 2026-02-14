80f0ed699fdfe70f0cde0d89087bb6b5039cd5c3
Akanksha7890 <akankshaupadhyay136@gmail.com>
2026-02-14 18:52:26 +0530

    Refactor BASE_URL for dynamic backend configuration
    Updated BASE_URL to use the production backend URL for deployment.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-"https://thinkboard-backend-gapx.onrender.com";
+// Dynamic backend URL
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // Local backend
+    : "https://thinkboard-backend-gapx.onrender.com/api"; // Production backend (Render)
+
 const api = axios.create({
   baseURL: BASE_URL,
 });
